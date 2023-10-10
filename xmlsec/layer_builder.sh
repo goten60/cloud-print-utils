048ed493b72134c73e16751075d917a955bd0403
JM <goten60@gmail.com>
2023-10-10 13:15:15 -0300

    update

--- xmlsec/layer_builder.sh
+++ xmlsec/layer_builder.sh
@@ -6,10 +6,11 @@ set -e
 yum -y install libxml2-devel xmlsec1-devel xmlsec1-openssl-devel libtool-ltdl-devel gcc
 
 mkdir -p "/opt/python/lib/python3.11/site-packages"
+mkdir -p "/opt/lib"
 python3 -m pip install xmlsec -t "/opt/python/lib/python3.11/site-packages"
 
-cp `rpm -ql xmlsec1 | grep "libxmlsec1.so.1$"` "/opt/lib"
-cp `rpm -ql xmlsec1-openssl | grep "libxmlsec1-openssl.so$"` "/opt/lib"
+cp `rpm -ql xmlsec1 | grep "libxmlsec1.so.1$"` "/opt/lib/"
+cp `rpm -ql xmlsec1-openssl | grep "libxmlsec1-openssl.so$"` "/opt/lib/"
 
 cd /opt
 zip -r9 /out/layer.zip lib/* python/*
